64dd7686c3c9805d7c7de277a7375c82757d584a
Agash1424 <agashdeepesh@gmail.com>
2025-09-21 16:42:04 +0530

    Add files via upload

---
+++ tortise.py
@@ -0,0 +1,11 @@
+import turtle
+window = turtle .Screen()
+window.title('Absolute positioning')
+the_turtle=turtle.getturtle()
+the_turtle.pensize(10)
+the_turtle.hideturtle()
+the_turtle.setposition(100,0)
+the_turtle.setposition(100,100)
+the_turtle.setposition(0,100)
+the_turtle.setposition(0,0)
+
